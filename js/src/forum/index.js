04251ddb9fee9102a3f9b5950504947adef1c419
Marco Colia <info@justoverclock.it>
2022-05-08 17:11:23 +0200

    dompurify added

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,13 +1,14 @@
 import app from 'flarum/forum/app';
-import {extend} from 'flarum/common/extend';
+import { extend } from 'flarum/common/extend';
 import DiscussionComposer from 'flarum/forum/components/DiscussionComposer';
-import Tooltip from 'flarum/common/components/Tooltip';
+import DOMPurify from 'dompurify';
 
 app.initializers.add('justoverclock/check-duplicate-discussions', () => {
   extend(DiscussionComposer.prototype, 'oncreate', function () {
     const numberOfSimilar = app.forum.attribute('justoverclock-check-duplicate-discussions.similarNumber') || 3;
     const composer = document.querySelector('.ComposerBody-editor');
     const inputTitle = document.querySelector('li.item-discussionTitle > h3 > input');
+
     let timeout = null;
 
     function clearSimilarDiscussions() {
@@ -19,46 +20,55 @@ app.initializers.add('justoverclock/check-duplicate-discussions', () => {
       const checkTitle = app.translator.trans('justoverclock-check-duplicate-discussions.forum.title');
       const desc = app.translator.trans('justoverclock-check-duplicate-discussions.forum.foundDiscussion');
 
-      let simDisc = app.store
+      app.store
         .find('discussions', {
-          page: {limit: numberOfSimilar},
-          'filter[q]': title,
+          page: { limit: numberOfSimilar },
+          'filter[q]': title.toLowerCase(),
         })
         .then((res) => {
-          if (inputTitle.value === '') return;
+          if (title === '') return;
           if (res.length > 0) {
             const container = document.createElement('div');
             container.setAttribute('class', 'simdisc-container');
             container.setAttribute('id', 'sim-container');
-            container.innerHTML = `
-                      <div>
-                         <p class="simtitle" >${checkTitle} </p>
-                         <p class="simtitle-desc">${desc}</p>
-                      </div>
-            `;
+            const html = `<div>
+                             <p class="simtitle" >${checkTitle} </p>
+                             <p class="simtitle-desc">${desc}</p>
+                          </div>`;
+
+            container.innerHTML = DOMPurify.sanitize(html, { USE_PROFILES: { html: true } });
 
             res.forEach((sim) => {
               const div = document.createElement('div');
               div.setAttribute('id', 'similar-discussions');
               div.setAttribute('class', 'similar-discussions');
-              let baSimilar = sim.data.attributes.hasBestAnswer
+              let baSimilar = sim.data.attributes.hasBestAnswer;
 
-              if (baSimilar !== false){
-                baSimilar = `<i class="fas fa-check-circle simsolved"></i> ${app.translator.trans('justoverclock-check-duplicate-discussions.forum.solvedSimilar')}`
+              if (baSimilar !== false) {
+                baSimilar = `<i class="fas fa-check-circle simsolved"></i> ${app.translator.trans(
+                  'justoverclock-check-duplicate-discussions.forum.solvedSimilar'
+                )}`;
               } else {
-                baSimilar = ''
+                baSimilar = '';
               }
-
-              div.innerHTML = `
-                  <div class="simdisc">
-                     <li>
-                        <i class="fas fa-exclamation sim"></i>
-                        <a class="simlink" href=${app.route.discussion(sim)} rel="nofollow" title="${sim.firstPost().contentHtml().replace(/<\/?[^>]+(>|$)/g, '').substr(0, 200) + '...'}">
-                            ${sim.data.attributes.title}
-                        </a>
-                        <span class="bestAnswer-similar" id="best-similar">${baSimilar}</span>
-                     </li>
-                  </div>`;
+              const resultHtml = `<div class="simdisc">
+                                    <li>
+                                        <i class="fas fa-exclamation sim"></i>
+                                             <a class="simlink" href=${app.route.discussion(sim)}
+                                                rel="nofollow"
+                                                title="${
+                                                  sim
+                                                    .firstPost()
+                                                    .contentHtml()
+                                                    .replace(/<\/?[^>]+(>|$)/g, '')
+                                                    .substr(0, 200) + '...'
+                                                }">
+                                                    ${sim.data.attributes.title}
+                                             </a>
+                                        <span class="bestAnswer-similar" id="best-similar">${baSimilar}</span>
+                                    </li>
+                                  </div>`;
+              div.innerHTML = DOMPurify.sanitize(resultHtml, { USE_PROFILES: { html: true } });
               container.appendChild(div);
               composer.insertAdjacentElement('beforebegin', container);
             });
@@ -69,7 +79,7 @@ app.initializers.add('justoverclock/check-duplicate-discussions', () => {
     inputTitle.addEventListener('keyup', (e) => {
       clearTimeout(timeout);
       timeout = setTimeout(function () {
-        searchForSimilarDiscussions(e.target.value);
+        searchForSimilarDiscussions(e.target.value.toLowerCase());
       }, 1000);
       clearSimilarDiscussions();
     });
